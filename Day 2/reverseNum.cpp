1866f3d966e33d029965a859186e54822ecc6335
ShrutiNitalikar <shrutinitalikar@gmail.com>
2025-10-20 12:38:36 +0530

    Day 2

---
+++ Day 2/reverseNum.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int reverseNum(int n){
+    int rev=0,temp=n,rem;
+    while(temp!=0){
+        rem = temp % 10;
+        rev = rev*10+rem;
+        temp = temp/10;
+
+    }
+    return rev;
+}
+
+int main(){
+    int num;
+    cout<<"Enter a number: ";
+    cin>>num;
+
+    int reversed=reverseNum(num);
+
+    cout<<"Reverse of "<<num<<" is: "<<reversed<<endl;
+
+    return 0;
+}
